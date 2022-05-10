486e158d7aa32ba2f8214d80c6095ade47e2315c
Jefwillems <jefwillems100@gmail.com>
2022-05-10 11:38:44 +0200

    fix: allow default store value

--- storage/__init__.py
+++ storage/__init__.py
@@ -7,14 +7,14 @@ class Storage:
     def __init__(self):
         self.stores: List[Store] = []
 
-    def add_memory_store(self, store_id: str):
-        self.stores.append(Store(id=store_id, storage_type='memory'))
+    def add_memory_store(self, store_id: str, data: dict = None):
+        self.stores.append(Store(id=store_id, storage_type='memory', data=data))
 
-    def add_local_store(self, store_id: str):
-        self.stores.append(Store(id=store_id, storage_type='local'))
+    def add_local_store(self, store_id: str, data: dict = None):
+        self.stores.append(Store(id=store_id, storage_type='local', data=data))
 
-    def add_session_store(self, store_id: str):
-        self.stores.append(Store(id=store_id, storage_type='session'))
+    def add_session_store(self, store_id: str, data: dict = None):
+        self.stores.append(Store(id=store_id, storage_type='session', data=data))
 
 
 container = Storage()
